ae23674d9d26f7e925aa5a936e7360f28e40fdd0
Raden Tonev <rado.toneff@gmail.com>
2016-11-18 13:05:00 -0500

    font changes and fixing scrolling

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('copy', function() {
 })
 
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['less', 'minify-css', /*'minify-js',*/ 'copy']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
