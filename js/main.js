5e089ad924c20e1c04c866ff7819cb8370c39c6d
Raden Tonev <rado.toneff@gmail.com>
2016-12-28 20:00:41 -0500

    splitting projects into separate files

--- js/main.js
+++ js/main.js
@@ -48,22 +48,28 @@ var slideIndex = 1;
 
     // Floating label headings for the contact form
     $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $("body").on("input propertychange", ".floating-label-form-group", function (e) {
             $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
+        }).on("focus", ".floating-label-form-group", function () {
             $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
+        }).on("blur", ".floating-label-form-group", function () {
             $(this).removeClass("floating-label-form-group-with-focus");
         });
 
         /**/
 
+            $('.portfolio-link').click(function (event) {
+                event.preventDefault();
+                var newLocation = this.href;
+                $('body').fadeOut(200, function () {
+                    window.location = newLocation;
+                });
+            });
 
 
+            /**/
 
-        /**/
 
-
-    });
-
+        });
+    // });
 })(jQuery); // End of use strict
