59a55ead7b74e8661790552f6993f3b2466510cc
Raden Tonev <rtonev@umich.edu>
2016-12-29 23:22:41 -0500

    updating vs3 project page

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-// Freelancer Theme JavaScript
-var slideIndex = 1;
-// showDivs(slideIndex);
-//
-//
-// function plusDivs(n) {
-//     showDivs(slideIndex += n);
-// }
-// function showDivs(n) {
-//     var i;
-//     var x = document.getElementsByClassName("mySlides");
-//     if (n > x.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = x.length}
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";
-//     }
-//     x[slideIndex-1].style.display = "inline";
-// }
+
 (function($) {
     "use strict"; // Start of use strict
 
@@ -72,4 +55,5 @@ var slideIndex = 1;
 
         });
     // });
+
 })(jQuery); // End of use strict
